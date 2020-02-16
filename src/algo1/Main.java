6afb3af3d58bf46ae77aeee9361b19e0d54d4e93
daniel m <daniel.martinezvillegas2001@gmail.com>
2020-02-16 10:38:59 -0500

    no

---
+++ src/algo1/Main.java
@@ -0,0 +1,24 @@
+package algo1;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("algo1.Main");
+
+	}
+	public void settings() {
+
+	}
+
+	public void setup() {
+
+	}
+
+	public void draw() {
+
+	}
+
+}
