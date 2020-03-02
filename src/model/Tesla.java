aeda565476f986ce9c1a12698dfaad54571e9f3b
Daniel Martinez villegas <53487916+Daniel1martinez2@users.noreply.github.com>
2020-03-02 16:28:51 -0500

    controlp5

--- src/model/Tesla.java
+++ src/model/Tesla.java
@@ -1,12 +1,16 @@
 package model;
 
 import processing.core.PApplet;
+import controlP5.*; 
 
 public class Tesla extends PApplet {
+	ControlP5 cp5; 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PApplet.main("model.Tesla"); 
+		
+		
 		
 
 	}
