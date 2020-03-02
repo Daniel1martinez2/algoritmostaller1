c6039ec6e098f4a9e168b926114e28662b52763f
Daniel Martinez villegas <53487916+Daniel1martinez2@users.noreply.github.com>
2020-03-02 13:28:53 -0500

    new one

---
+++ src/model/Tesla.java
@@ -0,0 +1,13 @@
+package model;
+
+import processing.core.PApplet;
+
+public class Tesla extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("model.Tesla"); 
+
+	}
+
+}
