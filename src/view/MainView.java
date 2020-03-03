b081426c278b677669fb44b1f233262a2dad4de6
daniel m <daniel.martinezvillegas2001@gmail.com>
2020-03-03 11:00:20 -0500

    importo como objetos en el mainview

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,10 +1,34 @@
 package view;
 
+
 import processing.core.PApplet;
 import controlP5.*; 
 
+
+import view.CarCharView;
+import view.CarCompareView;
+import view.CompraView;
+import view.EnvioView;
+import view.HistorialView;
+import view.HomeView;
+import view.ResumenView;
+
+import view.Sc1LoginView;
+import view.Sc1SigninView;
+
 public class MainView extends PApplet {
 	private ControlP5 cp5;
+	private CarCharView carcharview; 
+	private CarCompareView carcompareview;
+	private CompraView compraview; 
+	private EnvioView envioview; 
+	private HistorialView historialview; 
+	private HomeView homeview; 
+	private ResumenView resumeview; 
+	private Sc1LoginView sc1loginview; 
+	private Sc1SigninView sc1signinview; 
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PApplet.main("view.MainView");
@@ -12,17 +36,23 @@ public class MainView extends PApplet {
 	}
 	
 	public void settings() {
-		size(500, 500);
+		size(300, 600);
 
 	}
 
 	public void setup() {
 		cp5 = new ControlP5(this);
-		// ahora un ejemplo
-		cp5.addButton("boo")
-		.setValue(0)
-		.setPosition(200, 200)
-		.setSize(400, 200);
+		carcharview = new CarCharView(); 
+		carcompareview = new CarCompareView();
+		compraview = new CompraView(); 
+		envioview = new EnvioView(); 
+		historialview = new HistorialView(); 
+		homeview = new HomeView(); 
+		resumeview = new ResumenView(); 
+		sc1loginview = new Sc1LoginView(); 
+		sc1signinview = new Sc1SigninView(); 
+		
+		
 
 	}
 
